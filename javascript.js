4b3bdb4d3ea63a00dfd6fc13fbb29d34129207bf
RaulPabianCruz <ito.pabian@gmail.com>
2023-07-17 17:06:18 -0700

    Add getComputerChoice function to js

--- javascript.js
+++ javascript.js
@@ -3,5 +3,18 @@ const choiceP = "PAPER";
 const choiceS = "SCISSORS";
 
 function getComputerChoice(){
-    
+    let randChoice = Math.floor(Math.random() * 3) + 1;
+    let compChoice;
+    switch(randChoice) {
+        case 1: 
+            compChoice = choiceR;
+            break;
+        case 2:
+            compChoice = choiceP;
+            break;
+        default:
+            compChoice = choiceS;
+            break;
+    }
+    return compChoice;
 }
